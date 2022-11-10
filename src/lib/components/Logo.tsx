7c792d58e18cbf07c454c46b3d88da72f0d17aa2
Issi <issi.dev.iobroker@gmail.com>
2022-11-10 19:11:09 +0100

    Disable logo export

--- src/lib/components/Logo.tsx
+++ src/lib/components/Logo.tsx
@@ -1,56 +1,58 @@
-import LogoRA from "@iobroker/adapter-react-v5/components/Logo";
-import React from "react";
-import { useDialogs } from "../hooks/useDialogs";
-import { useGlobals } from "../hooks/useGlobals";
-import { useIoBrokerObject } from "../hooks/useIoBrokerObject";
-
-interface LogoProps {
-	classes?: { buttons?: string; logo?: string };
-}
-
-export const Logo: React.FC<LogoProps> = (props): JSX.Element => {
-	const { instance, namespace } = useGlobals();
-	const { showNotification } = useDialogs();
-	const [myObject, setObject] = useIoBrokerObject(
-		`system.adapter.${namespace}`,
-		{
-			subscribe: false,
-		},
-	);
-
-	const errorNotification = React.useCallback(
-		(error) => {
-			showNotification(error, "error");
-		},
-		[showNotification],
-	);
-	const successNotification = React.useCallback(
-		(msg) => {
-			showNotification(msg, "success");
-		},
-		[showNotification],
-	);
-
-	const handleLoadConfig = (native: ioBroker.AdapterConfig) => {
-		if (myObject)
-			setObject({
-				...myObject,
-				native,
-			});
-		successNotification("Configuration loaded");
-	};
-
-	return (
-		<LogoRA
-			instance={instance}
-			common={myObject ? myObject.common : {}}
-			native={myObject ? myObject.native : {}}
-			onError={(error: string) => errorNotification(error)}
-			onLoad={(native) => handleLoadConfig(native)}
-			classes={{
-				buttons: props.classes?.buttons ?? "",
-				logo: props.classes?.logo ?? "",
-			}}
-		/>
-	);
-};
+// import LogoRA from "@iobroker/adapter-react-v5/components/Logo";
+// import React from "react";
+// import { useDialogs } from "../hooks/useDialogs";
+// import { useGlobals } from "../hooks/useGlobals";
+// import { useIoBrokerObject } from "../hooks/useIoBrokerObject";
+//
+// interface LogoProps {
+// 	classes?: { buttons?: string; logo?: string };
+// }
+//
+// export const Logo: React.FC<LogoProps> = (props): JSX.Element => {
+// 	const { instance, namespace } = useGlobals();
+// 	const { showNotification } = useDialogs();
+// 	const [myObject, setObject] = useIoBrokerObject(
+// 		`system.adapter.${namespace}`,
+// 		{
+// 			subscribe: false,
+// 		},
+// 	);
+//
+// 	const errorNotification = React.useCallback(
+// 		(error) => {
+// 			showNotification(error, "error");
+// 		},
+// 		[showNotification],
+// 	);
+// 	const successNotification = React.useCallback(
+// 		(msg) => {
+// 			showNotification(msg, "success");
+// 		},
+// 		[showNotification],
+// 	);
+//
+// 	const handleLoadConfig = (native: ioBroker.AdapterConfig) => {
+// 		if (myObject)
+// 			setObject({
+// 				...myObject,
+// 				native,
+// 			});
+// 		successNotification("Configuration loaded");
+// 	};
+//
+// 	return (
+// 		<LogoRA
+// 			instance={instance}
+// 			common={myObject ? myObject.common : {}}
+// 			native={myObject ? myObject.native : {}}
+// 			onError={(error: string) => errorNotification(error)}
+// 			onLoad={(native: ioBroker.AdapterConfig) =>
+// 				handleLoadConfig(native)
+// 			}
+// 			classes={{
+// 				buttons: props.classes?.buttons ?? "",
+// 				logo: props.classes?.logo ?? "",
+// 			}}
+// 		/>
+// 	);
+// };
