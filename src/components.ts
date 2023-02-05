3c02ce752c83970d03dafcbd03c43d8c1dfd72c7
Issi <issi.dev.iobroker@gmail.com>
2023-02-05 21:14:55 +0100

    remove ip / number / Password Inputs

--- src/components.ts
+++ src/components.ts
@@ -3,10 +3,6 @@ export {
 	DropdownOption,
 	DropdownProps,
 } from "./lib/components/Dropdown";
-export {
-	IpAddressInput,
-	IpAddressInputProps,
-} from "./lib/components/IpAddressInput";
 export { default as Loader, LoaderProps } from "./lib/components/Loader";
 export { Logo } from "./lib/components/Logo";
 export {
@@ -14,12 +10,6 @@ export {
 	ModalDialogProps,
 	ModalState,
 } from "./lib/components/ModalDialog";
-export { NumberInput, NumberInputProps } from "./lib/components/NumberInput";
-export {
-	PasswordColors,
-	PasswordInput,
-	PasswordInputProps,
-} from "./lib/components/PasswordInput";
 export {
 	TreeColumnProps,
 	TreeDataProps,
